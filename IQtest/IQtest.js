6710a492d70016a719df8d5f6f60e5449edd4731
Emely <mly.molina.minaya@gmail.com>
2019-01-26 10:46:28 -0800

    IQtest new challenge

---
+++ IQtest/IQtest.js
@@ -0,0 +1,23 @@
+// Welcome to IQ-Test.
+// This is the first IQ test.
+
+// This is a reverse challenge, enjoy!
+
+// [execution time limit] 4 seconds (js)
+
+// [input] array.array.string test
+
+// Guaranteed constraints:
+// 2 ≤ test.length ≤ 50
+// 2 ≤ test[i].length ≤ 50
+// 0 ≤ test[i][j].length ≤ 6
+// test[i][j][k] ∈ {"0" - "9", "*", "/", "+", "-", "=", "?"}
+
+// [output] integer
+
+// A reverse challenge is a special type of challenge, where no description is provided! You have to solve two challenges in one: 
+// find out what the author wants from you and write a solution. Check out this page for more information.
+
+function IQ_TestI(test) {
+
+}
