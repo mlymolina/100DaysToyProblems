c603ec7030692a196a6bc983f31796a08023d3d0
Emely <mly.molina.minaya@gmail.com>
2019-01-29 15:49:38 -0800

    19 tests solved, need to improve runtime

--- almostIncreasingSequence/almostIncreasingSequence.js
+++ almostIncreasingSequence/almostIncreasingSequence.js
@@ -1,18 +1,22 @@
-// Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
+// Given a sequence of integers as an array, determine whether it is possible to obtain 
+// a strictly increasing sequence by removing no more than one element from the array.
 
-// Note: sequence a0, a1, ..., an is considered to be a strictly increasing if a0 < a1 < ... < an. Sequence containing only one element is also considered to be strictly increasing.
+// Note: sequence a0, a1, ..., an is considered to be a strictly increasing if a0 < a1 < ... < an. 
+//Sequence containing only one element is also considered to be strictly increasing.
 
 // Example
 
 // For sequence = [1, 3, 2, 1], the output should be
 // almostIncreasingSequence(sequence) = false.
 
-// There is no one element in this array that can be removed in order to get a strictly increasing sequence.
+// There is no one element in this array that can be removed in order to get a strictly increasing 
+// sequence.
 
 // For sequence = [1, 3, 2], the output should be
 // almostIncreasingSequence(sequence) = true.
 
-// You can remove 3 from the array to get the strictly increasing sequence [1, 2]. Alternately, you can remove 2 to get the strictly increasing sequence [1, 3].
+// You can remove 3 from the array to get the strictly increasing sequence [1, 2]. 
+//Alternately, you can remove 2 to get the strictly increasing sequence [1, 3].
 
 // Input/Output
 
@@ -29,8 +33,24 @@
 // Return true if it is possible to remove one element from the array in order to get a strictly increasing sequence, otherwise return false.
 const test = require('../test.js');
 
-function almostIncreasingSequence(sequence) {
+function isIncreasingSequence(sequence) {
+  for (let j = 0; j < sequence.length - 1; j++) {
+    if (sequence[j] >= sequence[j + 1]) return false;
+  }
+  return true;
+}
 
+
+function almostIncreasingSequence(sequence) {
+  if(isIncreasingSequence(sequence)) return true;
+
+  for (let i = 0; i < sequence.length; i++){
+    var newSequence = sequence.slice(0);
+    newSequence.splice(i, 1);
+    if (isIncreasingSequence(newSequence)) return true;
+  }
+
+  return false;
 }
 
 var tests = [
