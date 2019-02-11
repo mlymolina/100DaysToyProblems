6dc6145089c724f5176969d3eaad6b45197eaa74
Emely <mly.molina.minaya@gmail.com>
2019-02-10 22:32:13 -0800

    commonCharacterCount Added

---
+++ commonCharacterCount/commonCharacterCount.js
@@ -0,0 +1,63 @@
+// Given two strings, find the number of common characters between them.
+
+// Example
+
+// For s1 = "aabcc" and s2 = "adcaa", the output should be
+// commonCharacterCount(s1, s2) = 3.
+
+// Strings have 3 common characters - 2 "a"s and 1 "c".
+
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] string s1
+
+// A string consisting of lowercase English letters.
+
+// Guaranteed constraints:
+// 1 ≤ s1.length ≤ 15.
+
+// [input] string s2
+
+// A string consisting of lowercase English letters.
+
+// Guaranteed constraints:
+// 1 ≤ s2.length ≤ 15.
+
+// Reference: https://app.codesignal.com/arcade/intro/level-3/JKKuHJknZNj4YGL32
+
+function commonCharacterCount(s1, s2) {
+
+}
+
+
+// Input:
+// s1: "aabcc"
+// s2: "adcaa"
+// Expected Output:
+// 3
+
+// nput:
+// s1: "zzzz"
+// s2: "zzzzzzz"
+// Expected Output:
+// 4
+
+// Input:
+// s1: "abca"
+// s2: "xyzbac"
+// Expected Output:
+// 3
+
+// Input:
+// s1: "a"
+// s2: "b"
+// Expected Output:
+// 0
+
+
+// Input:
+// s1: "a"
+// s2: "aaa"
+// Expected Output:
